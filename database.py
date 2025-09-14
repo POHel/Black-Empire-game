9e1ee22672cbf2d94ff123386e365a4fbb7c3564
R3DAX-dot <alennikovvlad524@gmail.com>
2025-09-14 10:53:39 +0000

    update database

--- database.py
+++ database.py
@@ -203,16 +203,131 @@ CREATE TABLE IF NOT EXISTS legendaries(
                money_legendaries FLOAT NOT NULL,
                );
 ''')
+
 connect.commit()
 connect.close()
-connect = sqlite3.connect('data/items.db')
+connect = sqlite3.connect('data/white_items.db')
 cursor = connect.cursor()
 cursor.executescript('''
-CREATE TABLE IF NOT EXISTS test(
+CREATE TABLE IF NOT EXISTS island(
                id INTEGER PRIMARY KEY,
-               test_name TEXT NOT NULL,
-               test_fl FLOAT NOT NULL,
-               test_int INTEGER NOT NULL
+               name_island TEXT NOT NULL,
+               money_island FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS boosters(
+               id INTEGER PRIMARY KEY,
+               name_boosters TEXT NOT NULL,
+               money_boosters FLOAT NOT NULL,
+               time_boosters FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS NFT(
+               id INTEGER PRIMARY KEY,
+               name_NFT TEXT NOT NULL,
+               money_NFT FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS cars(
+               id INTEGER PRIMARY KEY,
+               name_cars TEXT NOT NULL,
+               money_cars FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS unique_items(
+               id INTEGER PRIMARY KEY,
+               name_unique_items TEXT NOT NULL,
+               money_unique_items FLOAT NOT NULL,
+               ); 
+                                         
+CREATE TABLE IF NOT EXISTS yacht(
+               id INTEGER PRIMARY KEY,
+               name_yacht TEXT NOT NULL,
+               money_yacht FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS airplanes(
+               id INTEGER PRIMARY KEY,
+               name_airplanes TEXT NOT NULL,
+               money_airplanes FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS residence(
+               id INTEGER PRIMARY KEY,
+               name_residence TEXT NOT NULL,
+               money_residence FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS jewelry(
+               id INTEGER PRIMARY KEY,
+               name_jewelry TEXT NOT NULL,
+               money_jewelry FLOAT NOT NULL,
+               );
+''')
+
+connect.commit()
+connect.close()
+connect = sqlite3.connect('data/black_items.db')
+cursor = connect.cursor()
+cursor.executescript('''
+CREATE TABLE IF NOT EXISTS guns(
+               id INTEGER PRIMARY KEY,
+               name_guns TEXT NOT NULL,
+               money_guns FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS substances(
+               id INTEGER PRIMARY KEY,
+               name_substances TEXT NOT NULL,
+               money_substances FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS medication(
+               id INTEGER PRIMARY KEY,
+               name_medication TEXT NOT NULL,
+               money_medication FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS contraband(
+               id INTEGER PRIMARY KEY,
+               name_contraband TEXT NOT NULL,
+               money_contraband FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS dangerous_services(
+               id INTEGER PRIMARY KEY,
+               name_dangerous_services TEXT NOT NULL,
+               money_dangerous_services FLOAT NOT NULL,
+               );   
+                                       
+CREATE TABLE IF NOT EXISTS cyber_implants(
+               id INTEGER PRIMARY KEY,
+               name_cyber_implants TEXT NOT NULL,
+               money_cyber_implants FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS gamble(
+               id INTEGER PRIMARY KEY,
+               name_gamble TEXT NOT NULL,
+               money_gamble FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS exotic(
+               id INTEGER PRIMARY KEY,
+               name_exotic TEXT NOT NULL,
+               money_exotic FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS rarities(
+               id INTEGER PRIMARY KEY,
+               name_rarities TEXT NOT NULL,
+               money_rarities FLOAT NOT NULL,
+               );
+                     
+CREATE TABLE IF NOT EXISTS legendaries(
+               id INTEGER PRIMARY KEY,
+               name_legendaries TEXT NOT NULL,
+               money_legendaries FLOAT NOT NULL,
                );
 ''')
 connect.commit()
